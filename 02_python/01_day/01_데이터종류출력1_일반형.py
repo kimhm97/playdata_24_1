0aa0fea43d917779c995e818dc439811b9eeece2
kyung yeol. in <inky4832@daum.net>
2022-09-15 09:54:14 +0900

    Add files via upload

---
+++ 02_python/01_day/01_데이터종류출력1_일반형.py
@@ -0,0 +1,18 @@
+
+# 일반데이터 출력
+print("정수: 양의정수", 10)
+print("정수: 음의정수", -10)
+print("정수: 0", 0)
+print("정수: 10진법(10표현)", 10)
+print("정수: 2진법", 0b10) # 2
+print("정수: 8진법", 0o10)  # 8
+print("정수: 16진법", 0x10) # 16
+print("실수: ", 3.14)
+print("실수(지수표기법: ", 3e+5) # 300000.0
+
+print("논리: ", True, False) # True/False가 아니어도 논리값으로 사용이 가능하다.
+
+def fun():
+    pass
+print("함수: ", fun )
+print("None: ", None ) # null 의미
