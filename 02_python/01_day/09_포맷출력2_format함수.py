3e2321de0568390e13c44e222b0200e37a7d8950
kyung yeol. in <inky4832@daum.net>
2022-09-15 16:09:33 +0900

    Add files via upload

--- 02_python/01_day/09_포맷출력2_format함수.py
+++ 02_python/01_day/09_포맷출력2_format함수.py
@@ -12,6 +12,8 @@
 
    3.  * key=value 형식 (매우 중요)
         변수 = "이름:{name}   나이:{age}   주소:{address}  ".format(name="홍길동",age=20, address="서울") 함수
+
+   4.  혼합 가능 (매우 중요)
 '''
 mesg = "이름:{} 나이:{} 주소:{}".format("홍길동",20,"서울")
 print(mesg)
@@ -21,4 +23,7 @@ print(mesg)
 mesg = "이름:{1} 나이:{0} 주소:{2}, {2}, {2}".format("홍길동",20,"서울")
 print(mesg)
 mesg = "이름:{name}   나이:{age}   주소:{address}  ".format(name="홍길동",age=20, address="서울")
+print(mesg)
+
+mesg = "이름:{0}   나이:{1}   주소:{address}  ".format("홍길동", 20, address="서울")
 print(mesg)
